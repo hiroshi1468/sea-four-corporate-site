eab4c0084f1255bc8adac8fbfbceb6451ac1c5bc
shoma-s-131 <shoma.s.works131@gmail.com>
2025-08-24 13:17:57 +0900

    サイトマップ設定ファイル

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+export async function GET() {
+  const baseUrl = "https://sea4-four.com";
+  // 必要に応じてページURLを追加
+  const pages = [
+    "",
+    "/company/profile",
+    "/company/PrivacyPolicy",
+    "/message",
+    "/contact",
+  ];
+
+  const urls = pages
+    .map(
+      (page) => `
+    <url>
+      <loc>${baseUrl}${page}</loc>
+    </url>
+  `
+    )
+    .join("");
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
