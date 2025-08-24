1a03d8e02d6d5bbfdd2d74a83251b3ffd95739de
shoma-s-131 <shoma.s.works131@gmail.com>
2025-08-24 14:14:21 +0900

    xmlファイルの修正

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -21,6 +21,9 @@ export async function GET() {
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://sea4-four.com/</loc>
+    </url>
     ${urls}
   </urlset>`;
 
